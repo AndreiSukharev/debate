78ce948adffb7f457a1375e643fb8ad50c942999
Andrei Sukharev <mr.andrey.sd@gmail.com>
2020-02-05 12:37:15 +0300

    added insert, select for routes

--- server/routes.go
+++ server/routes.go
@@ -6,13 +6,18 @@ import (
 )
 
 func getTasks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "get called"}`))
+	sqlString := "SELECT * FROM tasks;"
+	values := []string {""}
+	res := database.InsertToDB(sqlString, values)
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "ok"}`))
 }
 
 func signIn(w http.ResponseWriter, r *http.Request) {
-
+	sqlString := "SELECT password FROM users WHERE id=$1;"
+	values := []string {"1"}
+	res := database.InsertToDB(sqlString, values)
+	// todo: check password
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(`{"message": "ok"}`))
 }
@@ -20,6 +25,7 @@ func signIn(w http.ResponseWriter, r *http.Request) {
 func signUp(w http.ResponseWriter, r *http.Request) {
 	sqlString := "INSERT INTO users(login, password) VALUES($1, $2) returning id;"
 	values := []string {"log", "pass"}
+	// todo: check user exists hash password
 	database.InsertToDB(sqlString, values)
 	w.WriteHeader(http.StatusAccepted)
 	w.Write([]byte(`{"message": "ok"}`))
